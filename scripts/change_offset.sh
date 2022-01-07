b9bbcd6be8ff068a03cd56a5ea8310c6622a0d52
raditya budi handoko <radityabh@gmail.com>
2022-01-07 19:13:34 +0700

    Update change_offset.sh
    bug head.S

--- scripts/change_offset.sh
+++ scripts/change_offset.sh
@@ -2,6 +2,7 @@
 
 #-----------change offset-------------------
 
-loc='./arch/arm64/Makefile'
+loc='./arch/arm64'
 
-sed -i 's#0x00080000#0x01080000#g' $loc
+sed -i 's#0x00080000#0x01080000#g' "$loc/Makefile"
+sed -i 's#\#error TEXT_OFFSET must be less than 2MB#//\#error TEXT_OFFSET must be less than 2MB#g' "$loc/kernel/head.S"
