d6ac5fd660580f2cb333791c5ae682d009097e5b
raditya budi handoko <radityabh@gmail.com>
2022-01-07 15:20:36 +0700

    update
    script for add driver

---
+++ script/add_driver.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#-----------wifi driver-------------------
+
+wd='drivers/net/wireless/realtek'
+
+git clone -b rtl8189fs https://github.com/jwrdegoede/rtl8189ES_linux "$wd/rtl8189fs"
+git clone -b master https://github.com/jwrdegoede/rtl8189ES_linux "$wd/rtl8189es"
+git clone -b master https://github.com/kelebek333/rtl8188fu "$wd/rtl8188fu"
+git clone -b main https://github.com/radityabh/RTL8822CS "$wd/rtl8822cs"
+
+
+echo "obj-\$(CONFIG_RTL8189FS) += rtl8189fs/" >> "$wd/Makefile"
+echo "obj-\$(CONFIG_RTL8189ES) += rtl8189es/" >> "$wd/Makefile"
+echo "obj-\$(CONFIG_RTL8188FU) += rtl8188fu/" >> "$wd/Makefile"
+echo "obj-\$(CONFIG_RTL8822CS) += rtl8822cs/" >> "$wd/Makefile"
+
+sed -i 's#endif \# WLAN_VENDOR_REALTEK##g' "$wd/Kconfig"
+cat << 'EOF' >> "$wd/Kconfig"
+source "drivers/net/wireless/realtek/rtl8188fu/Kconfig"
+source "drivers/net/wireless/realtek/rtl8189es/Kconfig"
+source "drivers/net/wireless/realtek/rtl8189fs/Kconfig"
+source "drivers/net/wireless/realtek/rtl8822cs/Kconfig"
+
+endif # WLAN_VENDOR_REALTEK
+EOF
+
