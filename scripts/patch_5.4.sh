230da2b6bf509718f2c8008a3bb2b804ea79dfd2
raditya budi handoko <radityabh@gmail.com>
2022-06-06 10:01:56 +0700

    Update patch_5.4.sh
    change clock sdio to 100Mhz

--- scripts/patch_5.4.sh
+++ scripts/patch_5.4.sh
@@ -7,6 +7,8 @@ loc='./arch/arm64'
 sed -i 's#0x00080000#0x01080000#g' "$loc/Makefile"
 sed -i 's#\#error TEXT_OFFSET must be less than 2MB#//\#error TEXT_OFFSET must be less than 2MB#g' "$loc/kernel/head.S"
 
+sed -i 's#50000000#100000000#g' "./drivers/mmc/core/sdio.c"
+
 svn checkout https://github.com/radityabh/kernel-compiler/trunk/patch/5.4 patch
 
 for i in patch/*.patch; do patch -p1 < $i; done
